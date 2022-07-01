a81b34c47f9ef9b8e1d9fb8886a2ac0debc77ac0
Josip Ivica <josip.ivica@pm.me>
2022-07-01 23:45:22 +0200

    minor changes

---
+++ rust/minigrep/target/debug/deps/minigrep-2e2abc26e00bb39a.d
@@ -0,0 +1,5 @@
+/home/joseph/git/rust/minigrep/target/debug/deps/minigrep-2e2abc26e00bb39a.rmeta: src/main.rs
+
+/home/joseph/git/rust/minigrep/target/debug/deps/minigrep-2e2abc26e00bb39a.d: src/main.rs
+
+src/main.rs:
---
+++ rust/minigrep/target/debug/deps/minigrep-ca5d87738539ded3.d
@@ -0,0 +1,5 @@
+/home/joseph/git/rust/minigrep/target/debug/deps/minigrep-ca5d87738539ded3.rmeta: src/main.rs
+
+/home/joseph/git/rust/minigrep/target/debug/deps/minigrep-ca5d87738539ded3.d: src/main.rs
+
+src/main.rs:
