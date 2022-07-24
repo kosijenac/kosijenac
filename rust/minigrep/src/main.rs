c49a18dd9e8e33e4ad94a1219890004634e10f3a
Josip Ivica <josip.ivica@pm.me>
2022-07-24 17:04:25 +0200

    completing the minigrep

--- rust/minigrep/src/main.rs
+++ rust/minigrep/src/main.rs
@@ -4,15 +4,11 @@ use std::{env, process};
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|e| {
-        println!("Problem parsing args: {}", e);
+        eprintln!("Problem parsing args: {}", e);
         process::exit(1);
     });
-
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file);
-
     if let Err(e) = minigrep::run(config) {
-        println!("Critical error: {}", e);
+        eprintln!("Critical error: {}", e);
         process::exit(2);
     }
 }
