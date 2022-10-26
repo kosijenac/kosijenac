0e0a1e98dc1c5ecd06e352b5460166d8930b1cc7
Josip Ivica <josip.ivica@pm.me>
2022-10-26 14:30:13 +0200

    completed majority project

--- rust/majority/src/main.rs
+++ rust/majority/src/main.rs
@@ -1,9 +1,79 @@
 fn main() {
-    println!("Hello, world!");
+    let fruit = vec![
+        '🍎', '🍐', '🍊', '🍊', '🍎', '🍎', '🍎', '🍐', '🍐', '🍊', '🍐', '🍎', '🍎', '🍎', '🍎',
+    ];
+    let small: Vec<u8> = vec![
+        124, 0, 13, 51, 201, 42, 13, 43, 59, 73, 13, 13, 13, 92, 102, 43, 84, 13, 13, 28, 162, 11,
+    ];
+    let binary = vec![true, true, false, false, false, true, true, false, true];
+    let misc = vec!["🐔", "⭐", "🏳️‍⚧️", "🐱", "🌈", "🐶", "🏴‍☠️", "⚖️", "🏳️‍⚧️", "🛡️"];
+    println!(
+        "Kandidat za vecinu niza {:?} je {}, te on {} apsolutna vecina.",
+        fruit,
+        find_majority(&fruit).unwrap(),
+        match check_majority(&fruit, find_majority(&fruit).unwrap()) {
+            true => "je",
+            false => "nije",
+        }
+    );
+    println!(
+        "Kandidat za vecinu niza {:?} je {}, te on {} apsolutna vecina.",
+        small,
+        find_majority(&small).unwrap(),
+        match check_majority(&small, find_majority(&small).unwrap()) {
+            true => "je",
+            false => "nije",
+        }
+    );
+    println!(
+        "Kandidat za vecinu niza {:?} je {}, te on {} apsolutna vecina.",
+        binary,
+        find_majority(&binary).unwrap(),
+        match check_majority(&binary, find_majority(&binary).unwrap()) {
+            true => "je",
+            false => "nije",
+        }
+    );
+    println!(
+        "Kandidat za vecinu niza {:?} je {}, te on {} apsolutna vecina.",
+        misc,
+        find_majority(&misc).unwrap(),
+        match check_majority(&misc, find_majority(&misc).unwrap()) {
+            true => "je",
+            false => "nije",
+        }
+    );
 }
-fn find_majority<T: PartialEq>(votes: &Vec<T>) -> &T {
-    return &votes[0];
+/**
+ * Trazi `dominirajuci` element danog niza, tj. apsolutnu vecinu.
+ * Ako je niz prazan, vraca `None`, inace vraca kandidata za vecinu.
+ * Koristi https://en.wikipedia.org/wiki/Boyer%E2%80%93Moore_majority_vote_algorithm.
+ */
+fn find_majority<T: PartialEq>(votes: &Vec<T>) -> Option<&T> {
+    let mut counter = 0;
+    let mut element = match votes.get(0) {
+        Some(vote) => vote,
+        None => {
+            println!("Niz ne smije biti prazan!");
+            return None;
+        }
+    };
+    for vote in votes.iter() {
+        if counter == 0 {
+            counter = 1;
+            element = vote;
+        } else if *element == *vote {
+            counter += 1;
+        } else {
+            counter -= 1;
+        }
+    }
+    return Some(element);
 }
+/**
+ * Provjerava je li dani kandidat za apsolutnu vecinu zapravo apsolutna vecina.
+ * To radi tako da prebroji koliko se puta on pojavljuje u nizu.
+ */
 fn check_majority<T: PartialEq>(votes: &Vec<T>, candidate: &T) -> bool {
     let mut occurences = 0;
     for vote in votes.iter() {
---
+++ rust/majority/target/debug/deps/majority-9240da294cf34ff7.d
@@ -0,0 +1,5 @@
+/home/joseph/git/rust/majority/target/debug/deps/majority-9240da294cf34ff7.rmeta: src/main.rs
+
+/home/joseph/git/rust/majority/target/debug/deps/majority-9240da294cf34ff7.d: src/main.rs
+
+src/main.rs:
